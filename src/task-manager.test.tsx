3221d3810f8a591a35430fc652b737c559eb05d9
Milton Rodrigues <miltonrodrigues713@gmail.com>
2025-03-19 00:04:00 -0300

    Removed tests files from JS

--- src/task-manager.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import TaskManager from './task-manager';
-
-test('should render the project without errors', () => {
-  render(<TaskManager />);
-});
